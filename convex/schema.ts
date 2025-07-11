3b252d18c6f2af0d4392ec42ffbda052f06c53d1
balaji-ts-18 <balaji.ts.official@gmail.com>
2025-07-11 10:52:23 +0530

    connected db

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+import { defineSchema, defineTable } from "convex/server";
+
+import { v } from 'convex/values';
+
+
+export default defineSchema({
+
+    users: defineTable({
+        email: v.string(),
+        name: v.string(),
+        image: v.optional(v.string()),
+        role: v.union(v.literal("candidate"), v.literal("interviewer")),
+        clerkId: v.string(), 
+    }).index("byClerkId", ["clerkId"]),
+    
+}); 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -31,5 +35,7 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+
+    </ClerkProvider>
   );
 }
